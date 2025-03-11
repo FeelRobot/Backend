c18eb43c9e0b9413080f94739eb11bcee36f46ec
Taehun88 <taehun8235@gmail.com>
2025-03-11 16:05:24 +0900

    init

---
+++ src/main/java/com/feelrobot/feelrobot/FeelrobotApplication.java
@@ -0,0 +1,13 @@
+package com.feelrobot.feelrobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeelrobotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeelrobotApplication.class, args);
+	}
+
+}
