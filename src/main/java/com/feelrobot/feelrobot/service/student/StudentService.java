47a1dfdd086916c3293958a4e55bf959f145afd9
Taehun88 <taehun8235@gmail.com>
2025-05-04 19:37:13 +0900

    feat: get chatbot list for studentId

--- src/main/java/com/feelrobot/feelrobot/service/student/StudentService.java
+++ src/main/java/com/feelrobot/feelrobot/service/student/StudentService.java
@@ -1,4 +1,10 @@
 package com.feelrobot.feelrobot.service.student;
 
+import com.feelrobot.feelrobot.dto.student.ChatbotResponseDto;
+import com.feelrobot.feelrobot.exception.ResponseException;
+
+import java.util.List;
+
 public interface StudentService {
+    List<ChatbotResponseDto> getChatbotList(String studentId) throws ResponseException;
 }
--- src/main/java/com/feelrobot/feelrobot/service/student/StudentServiceImpl.java
+++ src/main/java/com/feelrobot/feelrobot/service/student/StudentServiceImpl.java
@@ -1,4 +1,53 @@
 package com.feelrobot.feelrobot.service.student;
 
-public class StudentServiceImpl {
+import com.feelrobot.feelrobot.dto.student.ChatbotResponseDto;
+import com.feelrobot.feelrobot.exception.ResponseException;
+import com.feelrobot.feelrobot.model.Student;
+import com.feelrobot.feelrobot.model.Study;
+import com.feelrobot.feelrobot.repository.ChatBotRepository;
+import com.feelrobot.feelrobot.repository.StudentRepository;
+import com.feelrobot.feelrobot.repository.StudyRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class StudentServiceImpl implements StudentService {
+
+    private final StudentRepository studentRepository;
+    private final StudyRepository studyRepository;
+    private final ChatBotRepository chatBotRepository;
+
+    @Override
+    public List<ChatbotResponseDto> getChatbotList(String studentId) throws ResponseException {
+        log.info("[getChatbotList] getChatbotList, input : {}", studentId);
+
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new ResponseException("존재하지 않는 아이디입니다.", 400));
+        try {
+            List<Study> studyList = student.getStudyList();
+            List<ChatbotResponseDto> responseDtoList = new ArrayList<>();
+            for(Study study : studyList) {
+                if(study.isDeleted()) continue;
+                String ChatBotName = chatBotRepository.findById(study.getChatBotId())
+                        .orElseThrow(() -> new RuntimeException("존재하지 않는 챗봇입니다."))
+                        .getChatBotName();
+                ChatbotResponseDto responseDto = ChatbotResponseDto.builder()
+                        .studyId(study.getStudyId())
+                        .chatBotName(ChatBotName)
+                        .chatBotId(study.getChatBotId())
+                        .build();
+                responseDtoList.add(responseDto);
+            }
+            return responseDtoList;
+        } catch (RuntimeException e) {
+            log.error("[getChatbotList] getChatbotList error {}", e.getMessage());
+            throw new RuntimeException(e.getMessage());
+        }
+    }
 }
